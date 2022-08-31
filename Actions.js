dc67a2ba9b1c1053dbd0c8b8d89c9477d2b53bd8
jeromerobichez <jerome.robichez@gmail.com>
2022-08-31 11:46:09 +0200

    creation of the getAllPlayers method and connected with the database

---
+++ Actions.js
@@ -0,0 +1,32 @@
+const { MongoClient } = require("mongodb");
+require('dotenv').config()
+const password = process.env.ATLAS_PWD
+const url = `mongodb+srv://jerrob:${password}@cluster0.qt9uckn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(url)
+const football = "football";
+
+// method to get the data of all the players
+const getAllPlayers = async () => {
+    let playersList = []
+    try {
+        await client.connect();
+        console.log("Connected correctly to server for get method");
+        const db = client.db(football);
+        const col = db.collection("players");
+        const dbResults = await col.find();
+        await dbResults.forEach((e, i) => playersList.push(e))
+       return playersList
+
+    } catch (err) {
+        console.log('errrrrrooooooor', err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+
+module.exports = {
+    getAllPlayers
+ }
+
--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 require('dotenv').config()
+const {
+  getAllPlayers
+} = require('./actions')
 
 const port = process.env.PORT
 app.listen(port, () => {
   console.log(`Hello we are listening on port ${port}`)
 })
 
-app.use(cors('*')) 
-app.use(express.json())
+app.use(cors('*')) // allow access to the server from any domain
+app.use(express.json()) // to make the server receive the incoming requests as JSON object
+
+app.get('/', async(req, res) => {
+  let allPlayers = await getAllPlayers()
+  res.set('Access-Control-Allow-Origin', '*')
+  res.send(allPlayers) 
+  })
